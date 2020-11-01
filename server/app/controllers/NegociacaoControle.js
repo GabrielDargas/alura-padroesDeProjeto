0119b5ce5b4132c3a770851ab6dafb72b902e5f8
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2020-11-01 19:55:05 -0300

    Aula 04

--- server/app/controllers/NegociacaoControle.js
+++ server/app/controllers/NegociacaoControle.js
@@ -6,6 +6,8 @@ class NegociacaoControle {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacoes = new ListaNegociacoes()
+
 
     }
 
@@ -13,20 +15,26 @@ class NegociacaoControle {
 
         event.preventDefault();
 
-        let data = new Date(...
-            this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
-            )
-        console.log(data)
+        this._listaNegociacoes.adiciona(this._criaNegociacao())
+        this._limpaformulario()
 
-        let negociacao = new Negociacao(
-            data,
+        console.log(this._listaNegociacoes.negociacoes)
+
+    }
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
-        ) 
+        )
+    }
 
-        console.log(negociacao)
-        //adicionar essa negociacao em uma lista 
+    _limpaformulario() {
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 }
