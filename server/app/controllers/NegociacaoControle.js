0f5154751f14a82416ca09ba65bc8c567487354f
GabrielDargas <68109894+GabrielDargas@users.noreply.github.com>
2020-11-04 14:09:25 -0300

    Aula 01 Parte 2

--- server/app/controllers/NegociacaoControle.js
+++ server/app/controllers/NegociacaoControle.js
@@ -7,7 +7,7 @@ class NegociacaoControle {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
-        this._listaNegociacoes = new ListaNegociacoes()
+        this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model)) 
 
         this._negociacoesView = new NegociacaoView($('#negociacoesView'))
         this._negociacoesView.update(this._listaNegociacoes)
@@ -21,9 +21,15 @@ class NegociacaoControle {
         event.preventDefault();
         this._listaNegociacoes.adiciona(this._criaNegociacao())
         this._mensagem.texto = 'Negociação adicionada com sucesso'
-        this._negociacoesView.update(this._listaNegociacoes)
         this._mensagemView.update(this._mensagem)
         this._limpaformulario()
+    }
+
+    apaga(){
+        this._listaNegociacoes.esvazia()
+
+        this._mensagem.texto = 'Negociações apagadas com sucesso'
+        this._mensagemView.update(this._mensagem)
     }
 
     _criaNegociacao() {
